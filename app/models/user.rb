76ec2d11dec46c602fd7dee769bed20d40a9160b
AmanVimal <amanvimal@gmail.com>
2017-08-02 16:53:03 +0530

    First Commit

---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  belongs_to :role
+  has_many :items
+  validates_presence_of :name
+
+  before_save :assign_role
+
+  after_create :send_admin_mail
+  def send_admin_mail
+    AdminMailer.new_user_waiting_for_approval(self).deliver
+  end
+
+  def assign_role
+ 		self.role = Role.find_by name: "Regular" if self.role.nil?
+ 	end
+
+ 	def admin?
+ 		self.role.name == "Admin"
+ 	end
+ 	
+ 	def seller?
+ 		self.role.name == "Seller"
+ 	end
+
+ 	def regular?
+ 		self.role.name == "Regular"
+ 	end
+ 	
+ 	def active_for_authentication?
+ 		super && approved?
+ 	end
+
+ 	def inactive_message
+ 		if !approved?
+ 			:not_approved
+ 		else
+ 			super
+ 		end
+ 	end
+
+ 	def self.send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    if !recoverable.approved?
+     recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+    elsif recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
+end
---
+++ db/migrate/20170801095904_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price, precision: 5, scale: 2
+      t.belongs_to :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
