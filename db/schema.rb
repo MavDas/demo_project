4d075456d513f66d9be4db7eff511e3708402862
AmanVimal <amanvimal@gmail.com>
2017-08-16 18:16:54 +0530

    Added pg and rails_12factor gem for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,13 @@ ActiveRecord::Schema.define(version: 20170809063722) do
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
-  
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -73,13 +79,5 @@ ActiveRecord::Schema.define(version: 20170809063722) do
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["role_id"], name: "index_users_on_role_id"
-
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,6 @@ r1 = Role.create({name: "Regular", description: "Can read items"})
 r2 = Role.create({name: "Seller", description: "Can read and create items. Can update and destroy own items"})
 r3 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
 
-u1 = User.create({name: "Sally", email: "sally@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r1.id})
-u2 = User.create({name: "Sue", email: "sue@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r2.id})
-u3 = User.create({name: "Kev", email: "kev@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r2.id})
-u4 = User.create({name: "Jack", email: "jack@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r3.id, approved: "true"})
 
-i1 = Item.create({name: "Rayban Sunglasses", description: "Stylish shades", price: 99.99, user_id: u2.id})
-i2 = Item.create({name: "Gucci watch", description: "Expensive timepiece", price: 199.99, user_id: u2.id})
-i3 = Item.create({name: "Henri Lloyd Pullover", description: "Classy knitwear", price: 299.99, user_id: u3.id})
-i4 = Item.create({name: "Porsche socks", description: "Cosy footwear", price: 399.99, user_id: u3.id})
+u1 = User.create({name: "Aman", email: "aman@hot.com", password: "password", password_confirmation: "password", role_id: r3.id, approved: "true", confirmed_at: Time.now.utc })
+
