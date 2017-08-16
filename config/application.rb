e5152eeea51ec4f562be1eecc864f4c541415a76
AmanVimal <amanvimal@gmail.com>
2017-08-16 17:08:37 +0530

    Added pg and rails_12factor gem for Heroku

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Store
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.timestamped_migrations = false
   end
 end
