30636aa0e3d58bdf392421b4e2259edef1f54a7e
AmanVimal <amanvimal@gmail.com>
2017-08-17 12:05:04 +0530

    facebook & google sign in

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.delivery_method = :smtp
 
-config.action_mailer.smtp_settings = {
-   address:              'smtp.gmail.com',
-   port:                 587,
-   domain:               'https://whispering-beach-71512.herokuapp.com',
-   user_name:            'amanvimal@gmail.com',
-   password:             'Gogetassj4',
-   authentication:       'plain',
-   enable_starttls_auto: true  
-}
 
 ActionMailer::Base.default_content_type = "text/html"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,16 @@ Rails.application.configure do
   config.log_level = :debug
   config.action_mailer.default_url_options = { :host => 'https://whispering-beach-71512.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "amanvimal@gmail.com",
+    :password => "Gogetassj4"
+  }
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
