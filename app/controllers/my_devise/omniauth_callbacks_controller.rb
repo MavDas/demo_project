cf2c6d2c11b62324a29bef997efe2b1aa43b658d
AmanVimal <amanvimal@gmail.com>
2017-08-18 13:33:10 +0530

    Minor Grammatical errors Fixed

--- app/controllers/my_devise/omniauth_callbacks_controller.rb
+++ app/controllers/my_devise/omniauth_callbacks_controller.rb
@@ -30,7 +30,7 @@ class MyDevise::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
   def all # all is Alias which will be called for facebook,google+ login
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?  # if user is found then he is logged in otherwise redirected to registration page
-      flash.notice = "Account Sccessfully authenticated."
+      flash.notice = "Account Successfully authenticated."
        sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes # if user is new then registration is done using omniauth
