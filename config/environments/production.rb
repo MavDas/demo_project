7e05bd3829fc2a069675ade0ae2174ee42166b5e
AmanVimal <amanvimal@gmail.com>
2017-08-18 12:40:54 +0530

    fixing email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,6 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "https://whispering-beach-71512.herokuapp.com",
