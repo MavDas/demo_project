415840bf5631244c6c9eeb5c7648cdee3a0ac194
AmanVimal <amanvimal@gmail.com>
2017-08-18 12:03:18 +0530

    facebook login fix

--- app/controllers/my_devise/omniauth_callbacks_controller.rb
+++ app/controllers/my_devise/omniauth_callbacks_controller.rb
@@ -31,10 +31,10 @@ class MyDevise::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?  # if user is found then he is logged in otherwise redirected to registration page
       flash.notice = "Account Sccessfully authenticated."
-      sign_in_and_redirect user
+       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.user_attributes"] = user.attributes # if user is new then registration is done using omniauth
-      redirect_to item_url
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
 
