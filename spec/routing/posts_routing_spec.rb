432c2e3e9ea8c12f1ffc37818b4b83ee2c857508
AmanVimal <amanvimal@gmail.com>
2017-10-06 13:23:08 +0530

    Added Controller spec and Routing spec

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -1,17 +1,17 @@
 RSpec.describe PostsController, type: :routing do
   describe 'routing' do
-    before(:each) { FactoryGirl.create(:group) }
+    let!(:group) { FactoryGirl.create(:group) }
 
 	  it 'routes to #index' do
-	    expect(get: '/groups/1/posts').to route_to('posts#index', group_id: "1")
+	    expect(get: "/groups/#{group.id}/posts").to route_to('posts#index', group_id: "#{group.id}")
 	  end
 
 	  it 'routes to #show' do
-	    expect(get: '/groups/1/posts/1').to route_to('posts#show',group_id: "1", id: '1')
+	    expect(get: "/groups/#{group.id}/posts/1").to route_to('posts#show',group_id: "#{group.id}", id: '1')
 	  end
 
 	  it 'routes to #new' do
-	    expect(get: '/groups/1/posts/new').to route_to('posts#new',group_id: "1")
+	    expect(get: "/groups/#{group.id}/posts/new").to route_to('posts#new',group_id: "#{group.id}")
 	  end
   end
 end
